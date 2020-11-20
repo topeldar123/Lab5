e9af66cff0f6932b9e1078614775d0ae47051a9b
topeldar123 <topeldar123@users.noreply.github.com>
2020-11-20 14:40:10 +0500

    )

---
+++ engine/connection/connection.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$user = 'f0473982_user';
+$password = 'root';
+$database = 'f0473982_Users';
+?>
---
+++ engine/page/title.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+	<head>
+        <?require_once 'head.php';?>
+        <title>
+            Кутушев Эльдар ПИ-319 Лабораторная работа №5
+        </title>
+	</head>
+</html>
